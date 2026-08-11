b5b4c4b7c656d1e14722f5c62e8ad34b3be69f13
Glenn <73631971+glennytoya@users.noreply.github.com>
2026-08-11 14:42:29 +0300

    Update Admin.ts

--- src/models/Admin.ts
+++ src/models/Admin.ts
@@ -1,23 +1,46 @@
-// src/models/Admin.ts
-import mongoose, { Schema, Document, Model } from 'mongoose'
+import mongoose, {
+  Schema,
+  Document,
+  Model,
+} from "mongoose";
 
 export interface IAdminDoc extends Document {
-  email: string
-  password: string
-  name: string
-  createdAt: Date
+  email: string;
+  password: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const AdminSchema = new Schema<IAdminDoc>(
   {
-    email:    { type: String, required: true, unique: true, trim: true, lowercase: true },
-    password: { type: String, required: true },
-    name:     { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-  { timestamps: true }
-)
+  {
+    timestamps: true,
+  }
+);
 
 const Admin: Model<IAdminDoc> =
-  mongoose.models.Admin || mongoose.model<IAdminDoc>('Admin', AdminSchema)
+  mongoose.models.Admin ||
+  mongoose.model<IAdminDoc>("Admin", AdminSchema);
 
-export default Admin
+export default Admin;
